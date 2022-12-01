5b8721c8d9d4895600228a1ccbababa3b391366d
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-01 16:46:16 +0000

    validar _n commit

--- src/app.js
+++ src/app.js
@@ -4,77 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+window.onload = function() {
+  document.querySelector("#btn").addEventListener("click", () => {
+    /*document.querySelector(#excuse).innerHTML = excuse;*/
+    document.querySelector("#My-resp").innerHTML = generaExcusa();
+  });
+};
 let generaExcusa = () => {
   let pronoun = ["the", "our", "meat", "floor", "pencil"];
   let adj = ["great", "big", "small", "fast", "flash"];
   let noun = ["jogger", "racoon", "geen", "red", "blue", ""];
   let exten = [".com", ".es", ".info", ".cl", ".org", ".net"];
 
-  let uno = parseInt(Math.floor(Math.random * pronoun.length));
-  let dos = parseInt(Math.floor(Math.random * adj.length));
-  let tres = parseInt(Math.floor(Math.random * noun.length));
-  let cuatro = parseInt(Math.floor(Math.random * exten.length));
-  return;
-  (pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro]);
+  let uno = parseInt(Math.floor(Math.random() * pronoun.length));
+  let dos = parseInt(Math.floor(Math.random() * adj.length));
+  let tres = parseInt(Math.floor(Math.random() * noun.length));
+  let cuatro = parseInt(Math.floor(Math.random() * exten.length));
+  return pronoun[uno] + adj[dos] + noun[tres] + exten[cuatro];
 };
-window.onload = function() {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#My-resp").innerHTML = generaExcusa();
-  });
-};
-//console.log("Hello Rigo from the console!");
-
-/* eslint-disable 
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-let generaExcusa = () => {
-  let pronoun = ["the", "our", "meat", "floor", "pencil"];
-  let adj = ["great", "big", "small", "fast", "flash"];
-  let noun = ["jogger", "racoon", "geen", "red", "blue", ""];
-  let exten = [".com", ".es", ".info", ".cl", ".org", ".net"];
-
-  let uno = parseInt(Math.floor(Math.random * pronoun.length));
-  let dos = parseInt(Math.floor(Math.random * adj.length));
-  let tres = parseInt(Math.floor(Math.random * noun.length));
-  let cuatro = parseInt(Math.floor(Math.random * exten.length));
-
-  return [
-    pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro]
-  ];
-};
-
-window.onload = function() {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#My-resp").innerHTML = generaExcusa();
-  });
-};*/
-//console.log("Hello Rigo from the console!");
-/* eslint-disable 
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#My-resp").innerHTML = generaExcusa();
-  });
-};
-let generaExcusa = () => {
-  let pronoun = ["the", "our", "meat", "floor", "pencil"];
-  let adj = ["great", "big", "small", "fast", "flash"];
-  let noun = ["jogger", "racoon", "geen", "red", "blue", ""];
-  let exten = ["com", "es", "info", "cl", "org", "net"];
-
-  let uno = Math.floor(Math.random * pronoun.length);
-  let dos = Math.floor(Math.random * adj.length);
-  let tres = Math.floor(Math.random * noun.length);
-  let cuatro = Math.floor(Math.random * exten.length);
-
-  return pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro];
-};
-*/
