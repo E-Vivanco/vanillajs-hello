7dbc44342d78a4524ee288d30f8afea047c05db0
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-01 14:46:02 +0000

    ...6 otro commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#My-resp").innerHTML = generaExcusa();
+  });
+};
+let generaExcusa = () => {
+  let pronoun = ['the','our','meat','floor','pencil'];
+  let adj = ['great', 'big','small','fast','flash' ];
+  let noun = ['jogger','racoon','geen','red','blue'];
+  let exten =['com','es','info','cl','org','net']
+
+  let uno= (Math.floor(Math.random * pronoun.length ))
+  let dos =(Math.floor(Math.random * adj.length ))
+  let tres=(Math.floor(Math.random * noun.length ))
+  let cuatro=(Math.floor(Math.random * exten.length ))
+
+  return pronoun[uno] +' '+adj[dos]+' '+noun[tres]+' '+exten[cuatro]
 };
