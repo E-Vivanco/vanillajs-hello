6f627d7211687074f4862d5a8dbefcae88b693d0
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-01 15:20:56 +0000

    11° commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,32 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let generaExcusa = () => {
+  let pronoun = ["the", "our", "meat", "floor", "pencil"];
+  let adj = ["great", "big", "small", "fast", "flash"];
+  let noun = ["jogger", "racoon", "geen", "red", "blue", ""];
+  let exten = ["com", "es", "info", "cl", "org", "net"];
+
+  let uno = Math.floor(Math.random * pronoun.length);
+  let dos = Math.floor(Math.random * adj.length);
+  let tres = Math.floor(Math.random * noun.length);
+  let cuatro = Math.floor(Math.random * exten.length);
+
+  return pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro];
+};
+
+window.onload = function() {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#My-resp").innerHTML = generaExcusa();
+  });
+};
+//console.log("Hello Rigo from the console!");
+/* eslint-disable 
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   document.querySelector("#btn").addEventListener("click", () => {
@@ -23,3 +49,4 @@ let generaExcusa = () => {
 
   return pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro];
 };
+*/
