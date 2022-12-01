aa79af7457e5f8e6b08389961e12700e79821fed
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-01 14:48:34 +0000

    7° commit

--- src/app.js
+++ src/app.js
@@ -11,15 +11,15 @@ window.onload = function() {
   });
 };
 let generaExcusa = () => {
-  let pronoun = ['the','our','meat','floor','pencil'];
-  let adj = ['great', 'big','small','fast','flash' ];
-  let noun = ['jogger','racoon','geen','red','blue'];
-  let exten =['com','es','info','cl','org','net']
+  let pronoun = ["the", "our", "meat", "floor", "pencil"];
+  let adj = ["great", "big", "small", "fast", "flash"];
+  let noun = ["jogger", "racoon", "geen", "red", "blue"];
+  let exten = ["com", "es", "info", "cl", "org", "net"];
 
-  let uno= (Math.floor(Math.random * pronoun.length ))
-  let dos =(Math.floor(Math.random * adj.length ))
-  let tres=(Math.floor(Math.random * noun.length ))
-  let cuatro=(Math.floor(Math.random * exten.length ))
+  let uno = Math.floor(Math.random * pronoun.length);
+  let dos = Math.floor(Math.random * adj.length);
+  let tres = Math.floor(Math.random * noun.length);
+  let cuatro = Math.floor(Math.random * exten.length);
 
-  return pronoun[uno] +' '+adj[dos]+' '+noun[tres]+' '+exten[cuatro]
+  return pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro];
 };
