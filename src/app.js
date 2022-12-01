1199a19d2516d2fc79cd70167cabcb76b75c37f3
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-01 15:26:22 +0000

    12° commit

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ let generaExcusa = () => {
   let noun = ["jogger", "racoon", "geen", "red", "blue", ""];
   let exten = ["com", "es", "info", "cl", "org", "net"];
 
-  let uno = Math.floor(Math.random * pronoun.length);
-  let dos = Math.floor(Math.random * adj.length);
-  let tres = Math.floor(Math.random * noun.length);
-  let cuatro = Math.floor(Math.random * exten.length);
+  let uno = parseInt(Math.floor(Math.random * pronoun.length));
+  let dos = parseInt(Math.floor(Math.random * adj.length));
+  let tres = parseInt(Math.floor(Math.random * noun.length));
+  let cuatro = parseInt(Math.floor(Math.random * exten.length));
 
   return pronoun[uno] + " " + adj[dos] + " " + noun[tres] + " " + exten[cuatro];
 };
